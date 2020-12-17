1c9fcdefa7ba5442fa5fab8662987d4aee567d8c
Steven Jackson <scjackson@pm.me>
2020-12-17 11:57:44 -0500

    Setup initial structs for credits and debits

--- main.go
+++ main.go
@@ -13,20 +13,41 @@ func main()  {
 	todaysDate := time.Now()
 	fmt.Println(todaysDate)
 
+	// Fetch today's budget information.
+	getTodaysBudget()
+}
+
+type Credit struct {
+	title string
+	amount float32
+}
+
+type Debit struct {
+	title string
+	amount float32
+}
+
+func getTodaysBudget() {
 	// Get today's starting balance.
-	startingBalance := 5324.14
+	startingBalance := float32(1234.56)
 	fmt.Println("Starting balance: ", startingBalance)
 
 	// Get today's credits.
-	todaysCredits := 3000.00
+	todaysCredits := Credit{
+		title:  "Person 1 Paycheck",
+		amount: 1000,
+	}
+
 	fmt.Println("Today's income:", todaysCredits)
 
 	// Get today's debits.
-	todaysDebits := 1700.00
+	todaysDebits := Debit{
+		title:  "Expense 1",
+		amount: 100,
+	}
 	fmt.Println("Today's expenses:", todaysDebits)
 
 	// Today's ending balance.
-	endingBalance := startingBalance - todaysDebits + todaysCredits
+	endingBalance := startingBalance - todaysDebits.amount + todaysCredits.amount
 	fmt.Println("Today's ending balance:", endingBalance)
-
 }
